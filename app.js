173e47f2e761a69aecf7a8f927859019d055707c
myoung8223 <141894282+myoung8223@users.noreply.github.com>
2026-06-27 15:37:33 -0500

    Update app.js
    Credits section updated.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ---- BUILD VERSION CONTROLLER ----
-const BUILD_NUMBER = "274"; // <-- Incremented for SVG Import Database & Grid Layout
+const BUILD_NUMBER = "275"; // <-- Incremented for SVG Import Database & Grid Layout
 
 import OpenSCAD from './libs/openscad.js';
 
